be17f46ff87e24ec1dcb05dc90240d614361e33c
Rafał Sojecki <rafal.sojecki@schibsted.com>
2019-10-23 12:12:28 +0200

    mocking command

--- src/index.js
+++ src/index.js
@@ -38,56 +38,19 @@ app.get('/', (req, res) => {
  */
 app.post('/command', (req, res) => {
   // extract the slash command text, and trigger ID from payload
-  const { text, trigger_id } = req.body;
+  const { text, trigger_id, channel_id } = req.body;
 
   // Verify the signing secret
   if (signature.isVerified(req)) {
-    // create the dialog payload - includes the dialog structure, Slack API token,
-    // and trigger ID
-    const dialog = {
+    const data = {
       token: process.env.SLACK_ACCESS_TOKEN,
       trigger_id,
-      dialog: JSON.stringify({
-        title: 'Submit a helpdesk ticket',
-        callback_id: 'submit-ticket',
-        submit_label: 'Submit',
-        elements: [
-          {
-            label: 'Title',
-            type: 'text',
-            name: 'title',
-            value: text,
-            hint: '30 second summary of the problem',
-          },
-          {
-            label: 'Description',
-            type: 'textarea',
-            name: 'description',
-            optional: true,
-          },
-          {
-            label: 'Urgency',
-            type: 'select',
-            name: 'urgency',
-            options: [
-              { label: 'Low', value: 'Low' },
-              { label: 'Medium', value: 'Medium' },
-              { label: 'High', value: 'High' },
-            ],
-          },
-        ],
-      }),
+      channel: channel_id,
+      text: 'lorem ipsum'
     };
-
-    // open the dialog by calling dialogs.open method and sending the payload
-    axios.post(`${apiUrl}/dialog.open`, qs.stringify(dialog))
-      .then((result) => {
-        debug('dialog.open: %o', result.data);
-        res.send('');
-      }).catch((err) => {
-        debug('dialog.open call failed: %o', err);
-        res.sendStatus(500);
-      });
+    request.post('https://slack.com/api/chat.postMessage', data, function (error, response, body) {
+      res.json();
+    });
   } else {
     debug('Verification token mismatch');
     res.sendStatus(404);
