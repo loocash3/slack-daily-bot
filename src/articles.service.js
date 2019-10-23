3aee5f1b1958f3cd94f5b36c578efa8174c37065
Rafał Sojecki <rafal.sojecki@schibsted.com>
2019-10-23 15:28:33 +0200

    Buttons test

--- src/articles.service.js
+++ src/articles.service.js
@@ -16,8 +16,77 @@ const send = (req, res) => {
         axios.post(`${apiUrl}/chat.postMessage`, qs.stringify(data))
             .then((result) => {
                 //Response text
-                const responseText = 'Mock text';
-                res.send(responseText);
+                const response = {
+                    blocks: [
+                        {
+                            type: 'section',
+                            text: {
+                                type: 'mrkdwn',
+                                text: '*Approval Request*\nYour approval is requested to make an offer to <http://example.com|Florence Tran>.'
+                            }
+                        },
+                        {
+                            type: 'context',
+                            elements: [
+                                {
+                                    type: 'mrkdwn',
+                                    text: '<http://example.com|View applicant>'
+                                }
+                            ]
+                        },
+                        {
+                            type: 'actions',
+                            elements: [
+                                {
+                                    type: 'button',
+                                    text: {
+                                        type: 'plain_text',
+                                        text: 'Approve',
+                                        emoji: true
+                                    }
+                                },
+                                {
+                                    type: 'button',
+                                    text: {
+                                        type: 'plain_text',
+                                        text: 'Reject',
+                                        emoji: true
+                                    }
+                                },
+                                {
+                                    type: 'overflow',
+                                    options: [
+                                        {
+                                            text: {
+                                                type: 'plain_text',
+                                                text: 'Follow',
+                                                emoji: true
+                                            },
+                                            value: 'value-0'
+                                        },
+                                        {
+                                            text: {
+                                                type: 'plain_text',
+                                                text: 'Activity feed',
+                                                emoji: true
+                                            },
+                                            value: 'value-1'
+                                        },
+                                        {
+                                            text: {
+                                                type: 'plain_text',
+                                                text: 'Details',
+                                                emoji: true
+                                            },
+                                            value: 'value-3'
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                };
+                res.json(response);
             }).catch((err) => {
                 debug('err: %o', err);
                 res.sendStatus(500);
